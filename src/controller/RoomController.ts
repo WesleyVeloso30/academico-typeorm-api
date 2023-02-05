403d8306199baf6f02d0090eb821a9a293ba9cb2
Wesley <wesleyvc2110@gmail.com>
2023-02-05 14:52:00 -0300

    fix: create video; feat: starting create subject

--- src/controller/RoomController.ts
+++ src/controller/RoomController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { RoomRepository } from "../repositories/RoomRepository";
+import { SubjectRepository } from "../repositories/SubjectRepository";
 import { VideoRepository } from "../repositories/VideoRepository";
 
 export class RoomController {
@@ -27,18 +28,41 @@ export class RoomController {
         if(!title || !url || !idRoom) throw new Error ('Preencha os campos obrigatorios.');
 
         try {
-            const room = RoomRepository.findOneBy({ id: Number(idRoom) });
+            const room = await RoomRepository.findOneBy({ id: Number(idRoom) });
 
-            if (!room) return res.status(404).json({message: 'Aula não existe.'})
+            if (!room) return res.status(404).json({message: 'Aula não existe.'});
 
-            // const newVideo = VideoRepository.create({ title, url,  });
+            const newVideo = VideoRepository.create({ title, url, room });
 
-            // await VideoRepository.save(newVideo);
+            await VideoRepository.save(newVideo);
 
-            // return res.status(201).json(newVideo);
+            return res.status(201).json(newVideo);
         } catch (error) {
             console.error(error);
-            return res.status(500).json({message: 'Internal Server Error'})
+            return res.status(500).json({message: 'Internal Server Error'});
+        }
+    }
+
+    async roomSubject(req: Request, res: Response) {
+        const { subject_id } = req.body;
+        const { idRoom } = req.params;
+
+        try {
+            const room = await RoomRepository.findOneBy({ id: Number(idRoom) });
+
+            if (!room) return res.status(404).json({message: 'Aula não existe.'});
+
+            const subject = await SubjectRepository.findOneBy({id: Number(subject_id)});
+
+            if (!subject) return res.status(404).json({message: 'Disciplina não existe.'});
+
+            await RoomRepository.update(idRoom, {
+                ...room,
+                subjects: [subject],
+            });
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({message: 'Internal Server Error'});
         }
     }
 }
